a2dbfcb25a45ff28710eb88d22e6bc0d4c4a7c85
Marina <skl.74@mail.ru>
2023-04-15 12:11:16 +0300

    first commit

---
+++ board/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.views import View
+from .tasks import hello, printer
+
+class IndexView(View):
+    def get(self, request):
+        # printer.apply_asinc([10], countdown=5)
+        hello.delay()
+        return HttpResponse('Hello!')
+
+
